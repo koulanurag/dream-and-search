3cefaf38bf801bc76e4d8cf805217b32e6e61324
Anurag Koul <koulanurag@gmail.com>
2020-10-25 02:04:48 -0700

    init-commit

--- config/box2d/__init__.py
+++ config/box2d/__init__.py
@@ -7,10 +7,10 @@ ENVS = ['LunarLanderContinuous-v2', 'BipedalWalker-v3', 'BipedalWalkerHardcore-v
 class Box2DConfig(BaseConfig):
     def __init__(self):
         super(Box2DConfig, self).__init__()
-        self.seed_steps = 5000
-        self.max_env_steps = 1000000
+        self.seed_steps = 1000
+        self.max_env_steps = 200000
         self.env_itr_steps = 1000
-        self.test_interval_steps = 10000
+        self.test_interval_steps = 5000
 
     def new_game(self, seed=None):
         env = GymEnv(self.args.env, self.args.symbolic_env, seed, self.args.max_episode_length,
